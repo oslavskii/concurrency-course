2edfcb1b96f95e3fec434254ae66459106cf8c07
oslavskii <oslavskiy@phystech.edu>
2022-08-10 20:30:48 +0300

    Add optimistic and pessimistic stoppable auctions

--- src/main/java/course/concurrency/exams/auction/AuctionStoppableOptimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionStoppableOptimistic.java
@@ -4,23 +4,24 @@ import java.util.concurrent.atomic.AtomicMarkableReference;
 
 public class AuctionStoppableOptimistic implements AuctionStoppable {
 
+    private static final Bid DEFAULT_BID = new Bid(0L, 0L, 0L);
+
     private Notifier notifier;
 
     public AuctionStoppableOptimistic(Notifier notifier) {
         this.notifier = notifier;
     }
 
-    private final AtomicMarkableReference<Bid> latestBid = new AtomicMarkableReference<>(null, false);
+    private final AtomicMarkableReference<Bid> latestBid = new AtomicMarkableReference<>(DEFAULT_BID, false);
 
     public boolean propose(Bid bid) {
         Bid latest;
         do {
             latest = latestBid.getReference();
-            if (latest != null && bid.price <= latest.price)
+            if (bid.price <= latest.price || latestBid.isMarked())
                 return false;
-        } while (latestBid.compareAndSet(latest, bid, false, false));
-        if (latest != null)
-            notifier.sendOutdatedMessage(latest);
+        } while (!latestBid.compareAndSet(latest, bid, false, false));
+        notifier.sendOutdatedMessage(latest);
         return true;
     }
 
--- src/main/java/course/concurrency/exams/auction/AuctionStoppablePessimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionStoppablePessimistic.java
@@ -2,25 +2,38 @@ package course.concurrency.exams.auction;
 
 public class AuctionStoppablePessimistic implements AuctionStoppable {
 
+    private static final Bid DEFAULT_BID = new Bid(0L, 0L, 0L);
+
     private Notifier notifier;
 
     public AuctionStoppablePessimistic(Notifier notifier) {
         this.notifier = notifier;
     }
 
-    private volatile Bid latestBid;
+    private volatile Bid latestBid = DEFAULT_BID;
     private volatile boolean finished = false;
 
-    public synchronized boolean propose(Bid bid) {
+    public boolean propose(Bid bid) {
         if (finished)
             return false;
-        if (latestBid == null || bid.price > latestBid.price) {
-            if (latestBid != null)
-                notifier.sendOutdatedMessage(latestBid);
-            latestBid = bid;
-            return true;
+
+        if (bid.price > latestBid.price) {
+            var outdated =  proposeExclusively(bid);
+            if (outdated != null) {
+                notifier.sendOutdatedMessage(outdated);
+                return true;
+            }
         }
         return false;
+    }
+
+    private synchronized Bid proposeExclusively(Bid bid) {
+        var outdated = latestBid;
+        if (bid.price > outdated.price) {
+            latestBid = bid;
+            return outdated;
+        }
+        return null;
     }
 
     public Bid getLatestBid() {
